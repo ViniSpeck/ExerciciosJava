4d1fffb81ea52f7c97597a43ad47e349fc093744
Vini Speck <vinicius.speck@catolicasc.org.br>
2020-09-16 21:07:45 -0300

    terceiro commit

--- Date/DateTest.java
+++ Date/DateTest.java
@@ -1,6 +1,6 @@
 public class DateTest {
 	public static void displayDate(Date d) {
-		System.out.printf("%d/%d/%d\n", d.getDay(), d.getMonth(), d.getYear());
+		System.out.printf("%02d/%02d/%d\n", d.getDay(), d.getMonth(), d.getYear());
 	}
 	public static void main(String[] args){
 		Date d1 = new Date(12,10,2001);
